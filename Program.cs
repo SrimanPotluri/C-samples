684d65ac2d8006dca895fd7f23d5a09b1b4dc781
SrimanPotluri <srimanpotluri@gmail.com>
2022-07-08 16:41:31 -0400

    Update Program.cs
    Updated to take the input from command line

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Drawing;
 using System.IO;
 using OfficeOpenXml;
@@ -9,39 +9,45 @@ public class RecursiveFileProcessor
 
     public static async Task Main(string[] args)
     {
-       
-       foreach(string path in args)
-       {
-            if (Directory.Exists(path))
-            {
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                var file = new FileInfo(@"C:\Users\potluris\FileNames.xlsx");
+        if(args == null || args.Length != 3)
+        {
+            throw new ArgumentException();
+        }
 
-                var filenames = ReadFileNamesFromDirectory(path);
+        var inputpath = args[0];
+        var outputpath = args[1];
+        var tag = args[2];
 
-                await SaveToExcelFile(filenames, file);
-            }
-            else
-            {
-                Console.WriteLine("{0} is not a directory.", path);
-            }
 
-       }
+        if (Directory.Exists(inputpath))
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var file = new FileInfo(outputpath);
+
+            var filenames = ReadFileNamesFromDirectory(inputpath);
+
+            await SaveToExcelFile(filenames, file, tag);
+        }
+        else
+        {
+            Console.WriteLine("{0} is not a directory.", inputpath);
+        }
+
     }
 
-    private static async Task SaveToExcelFile(List<string> filenames, FileInfo file)
+    private static async Task SaveToExcelFile(List<string> filenames, FileInfo file, string tag)
     {
         DeleteIfFileExists(file);
 
         using (var package = new ExcelPackage(file))
         {
-            var ws = package.Workbook.Worksheets.Add("Bride Album");
+            var ws = package.Workbook.Worksheets.Add(tag);
 
             var range = ws.Cells["A2"].LoadFromCollection(filenames);
             range.AutoFitColumns();
 
             //format the header
-            ws.Cells["A1"].Value = "Bride Album";
+            ws.Cells["A1"].Value = tag;
             ws.Column(col: 1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
             ws.Row(row: 1).Style.Font.Color.SetColor(Color.Blue);
 
